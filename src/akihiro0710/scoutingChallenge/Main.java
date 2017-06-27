ef3bff9fd227f27ecaa3bc6c3eef1e6535df1b37
akihiro0710 <acedia2074@gmail.com>
2017-06-27 23:04:36 +0900

    create main

---
+++ src/akihiro0710/scoutingChallenge/Main.java
@@ -0,0 +1,10 @@
+package akihiro0710.scoutingChallenge;
+
+/**
+ * Created by ta on 2017/06/27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
